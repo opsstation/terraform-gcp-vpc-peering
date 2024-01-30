a3eb9f3bcdf89a6849d5a11d1cf241965a533290
thekamleshyadav <thekamleshyadav26@gmail.com>
2024-01-30 16:16:56 +0530

    add github-actions

--- example/versions.tf
+++ example/versions.tf
@@ -1,18 +1,13 @@
 terraform {
-  required_version = ">= 0.14, < 2.0"
-
+  required_version = ">= 1.3.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.50, < 5.0"
+      version = ">= 4.75, < 5.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.1.0"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = "3.1.0"
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 4.75, < 5.13.0"
     }
   }
 }
--- versions.tf
+++ versions.tf
@@ -1,10 +1,13 @@
 terraform {
-  required_version = ">= 0.14, < 2.0"
-
+  required_version = ">= 1.3.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.50, < 5.0"
+      version = ">= 4.75, < 5.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 4.75, < 5.13.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -16,3 +19,5 @@ terraform {
     }
   }
 }
+
+
